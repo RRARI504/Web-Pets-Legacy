f142b353f55cdcf7448ad84d19def14ae22c4999
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-12-15 20:16:45 -0600

    render test React component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,8 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './components/App';
 
 // create root node and render <App />
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
