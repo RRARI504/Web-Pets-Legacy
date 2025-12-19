31f9efbaa90ef0838892e5897cba777c80b19066
azaria <azariaplayer@gmail.com>
2025-12-19 09:29:44 -0600

    DELETE and POST request handling created and correct. POST now checks if user has a pet already

--- server/routers/pet.js
+++ server/routers/pet.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
-const db = require('../db');
-const skills = require('../data/skills.js');
+const db = require("../db");
+const skills = require("../data/skills.js");
 
-
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // if user is signed in - we check the session to see if the passport exist
   const { passport } = req.session;
-  if(passport) {
+  if (passport) {
     // find the pet with the same userId
-    db.Pet.find({userId: passport.user.id})
+    db.Pet.find({ userId: passport.user.id })
       .then((pet) => {
         res.status(200).send(pet);
       })
@@ -19,44 +18,53 @@ router.get('/', (req, res) => {
         res.sendStatus(404);
       });
   } else {
-    res.redirect('/login'); // review and check endpoint later
+    res.redirect("/login"); // review and check endpoint later
   }
-
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const { passport } = req.session;
-  if(passport)  {
-    db.Pet.create({
-      userId: passport.user.id,
-      petName: req.body.petName,
-      training: skills.map((skill) => {
-        return {
-          name: skill.catergoryName,
-          stat: 0
-        };
-      }),
-      mood: 0,
-      love: 0,
-      health: 100
-    })
-    .then((pet) => {
-      res.status(201).send(pet);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(404);
-    });
+  if (passport) {
+    db.Pet.find({ userId: passport.user.id })
+      .then((pet) => {
+        if (pet) {
+          res.status(200).send("You already have a pet");
+        } else {
+          db.Pet.create({
+            userId: passport.user.id,
+            petName: req.body.petName,
+            training: skills.map((skill) => {
+              return {
+                name: skill.catergoryName,
+                stat: 0,
+              };
+            }),
+            mood: 0,
+            love: 0,
+            health: 100,
+          })
+            .then((pet) => {
+              res.status(201).send(pet);
+            })
+            .catch((err) => {
+              console.error(err);
+              res.sendStatus(500);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 });
 
-router.patch('/', (req, res) => {
-  db.Pet.find({userId: passport.user.id})
+router.patch("/", (req, res) => {
+  db.Pet.find({ userId: passport.user.id })
     .then((pet) => {
       // change the petName to the req.body.petName
-      
     })
     .catch((err) => {
       console.error(err);
@@ -65,13 +73,13 @@ router.patch('/', (req, res) => {
 });
 
 // delete
-router.delete('/', (req, res) => {
-  db.Pet.findByIdAndDelete({userId: passport.user.id})
+router.delete("/", (req, res) => {
+  db.Pet.findByIdAndDelete({ userId: passport.user.id })
     .then(() => {
       res.sendStatus(200);
     })
     .catch((err) => {
-      console.error('This pet does not exist', err);
+      console.error("This pet does not exist", err);
       res.sendStatus(404);
     });
 });
