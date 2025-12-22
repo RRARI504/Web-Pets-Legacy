d0ebd648eff08f7433ad31e04eb7235dab3cd7bb
azaria <azariaplayer@gmail.com>
2025-12-22 15:28:51 -0600

    User can now create a pet if they have not already

--- client/src/components/ScreenView.jsx
+++ client/src/components/ScreenView.jsx
@@ -1,25 +1,34 @@
-import {useState, useEffect} from 'react'
+import { useState } from 'react';
 import React from 'react';
 import axios from 'axios';
 
-const ScreenView = ({pet}) => {
- // see what pet is first - it is a pet object
-
-
-
+const ScreenView = ({ pet }) => {
+  const [petName, setPetName] = useState('');
   const handleSubmit = () => {
-    axios.post('/pet', {petName: 1})
+    if(petName === '') {
+      // if the user doesn't enter anything the button does nothing
+      return;
+    }
+    axios.post('/pet', { petName }) // fix
       .catch((err) => {
         console.error(err, 'coming from dummyPet');
       });
   };
 
-
+  if(pet.petName !== ''){
+    return (
+      <div>
+        <p>{pet.petName}</p>
+      </div>
+    );
+  }
+  
+  // this is for if the user does not have a pet
   return (
     <div style={{ border: '5px inset hotpink', height: '360px', margin: '5px', backgroundColor: 'lavender' }}>
       this is the screen - kitty goes here
-        <input type='text'/>
-        <button>Submit</button>
+        <input type='text' value={petName} onChange={(e) => setPetName(e.target.value)}/>
+        <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
