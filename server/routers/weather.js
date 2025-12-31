dac31902084acaa22ca6210714c415871ddfcf93
cfields05 <senken1218@gmail.com>
2025-12-31 11:37:31 -0600

    weather stuff I had messages for before but no longer do

--- server/routers/weather.js
+++ server/routers/weather.js
@@ -1,7 +1,35 @@
 
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-// router.get()
+const { Weather } = require('../db');
+
+
+const options = {
+  method: 'GET',
+  url: 'http://api.weatherapi.com/v1/current.json',
+  params: {
+    key: process.env['WEATHER_API_KEY'],
+    q: '70112',
+  }
+};
+
+const getWeather = async () => {
+  const weather = await axios(options);
+  return weather.data;
+};
+
+
+router.get('/', (req, res) => {
+  getWeather()
+    .then(weatherInfo => {
+      res.status(200).send(weatherInfo);
+    })
+    .catch(err => {
+      console.error('Unable to retrieve from weather api on server: ', err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
