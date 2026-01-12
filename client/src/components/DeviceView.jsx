cc81c69958ed32c961e312a52ff113ba12457d34
RRARI504 <ryansimien@ymail.com>
2026-01-12 10:32:16 -0600

    "fixing merge conflicts"

--- client/src/components/DeviceView.jsx
+++ client/src/components/DeviceView.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+
 
 import ScreenView from "./ScreenView";
 import DashboardView from "./Dashboard/DashboardView";
